f759ba8e54a603f61e27d934bbb448a06620409d
leewebstechnologies <aliuajibade317@gmail.com>
2022-12-07 15:48:18 -0800

    added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,17 +1,63 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setInputs((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }));
+  };
+
+  // console.log(inputs);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.post("/auth/register", inputs);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="auth">
       <h1>Register</h1>
       <form>
-        <input required type="text" placeholder="Username" />
-        <input required type="email" placeholder="Email" />        
-        <input required type="password" placeholder="Password" />
-        <button>Register</button>
+        <input
+          required
+          type="text"
+          placeholder="Username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="email"
+          placeholder="Email"
+          name="email"
+          onChange={handleChange}
+        />
+        <input
+          required
+          type="password"
+          placeholder="Password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Register</button>
         <p>This is an error!</p>
-        <span>Don't you have an account? <Link to="/login">Login</Link></span>
+        <span>
+          Don't you have an account? <Link to="/login">Login</Link>
+        </span>
       </form>
     </div>
   );
